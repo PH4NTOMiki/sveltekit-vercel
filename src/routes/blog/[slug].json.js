1c7510ac5c7ef6ebfa66980e2e988786e2251d30
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2021-10-27 17:10:37 +0200

    outside mongodb

--- src/routes/blog/[slug].json.js
+++ src/routes/blog/[slug].json.js
@@ -6,7 +6,7 @@ export async function get({ params }) {
 	// is called [slug].json.js
 	const { slug } = params;
 
-	let client = await mongo.MongoClient.connect("mongodb://localhost");
+	let client = await mongo.MongoClient.connect(process.env['VITE_MONGODB_URI'] || import.meta.env.VITE_MONGODB_URI);
     let db = client.db("sveltekit1");
 	let collection = db.collection('blogposts');
 	const article = await collection.findOne({ slug });
